c965981f0091a7f3e288049442c44cf97413d3f8
Mutt <mrmutt1406@gmail.com>
2016-07-19 11:00:35 +0300

    Add files via upload

---
+++ encryptfile/encryptfile/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Security;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using System.Text;
+namespace encryptfile
+{
+    //Enumartors for options
+    public enum CryptMethod { ENCRYPT, DECRYPT }
+    public enum CryptClass { AES, RC2, RC3, DES, TDS }
+    class Class1
+    {
+        
+        //The acutal crypting method
+        public object Crypt(CryptMethod _method, CryptClass _class,object _input,string _key)
+        {
+            //Using switch to pick a algorithem method.
+            //control is a algorithem. In the switch we change the method of control
+            SymmetricAlgorithm control;
+            switch (_class)
+            {
+                case CryptClass.AES:
+                    control = new AesManaged();
+                    break;
+                case CryptClass.RC2:
+                    control = new RC2CryptoServiceProvider();
+                    break;
+                case CryptClass.RC3:
+                    control = new RijndaelManaged();
+                    break;
+                case CryptClass.TDS:
+                    control = new TripleDESCryptoServiceProvider();
+                    break;
+                default:
+                    return false;
+                    break;
+
+            }
+
+            //Encryption key
+            control.Key = UTF8Encoding.UTF8.GetBytes(_key);
+            //Padding
+            control.Padding = PaddingMode.PKCS7;
+            //Ciphermode
+            control.Mode = CipherMode.ECB;
+
+            ICryptoTransform cTranfrom = null;
+            //The results in an byte array
+            byte[] resultArray;
+            //Checkes if we are encrypting or decrypting
+            if (_method == CryptMethod.ENCRYPT)
+            { cTranfrom = control.CreateEncryptor();}
+            else if (_method == CryptMethod.DECRYPT)
+            { cTranfrom = control.CreateDecryptor();}
+            //Checkes if the input is a string 
+            if (_input is string)
+            {
+                //Getting the input as a byte array but then converting to string
+                byte[] inputArray = UTF8Encoding.UTF8.GetBytes(_input as string);
+                //The final result array in a block
+                resultArray = cTranfrom.TransformFinalBlock(inputArray, 0, inputArray.Length);
+                //Clearing control
+                control.Clear();
+                //Converting to a string
+                return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+
+            }
+            //Checkes if the input is a byte
+            else if(_input is byte[])
+            {
+                //Getting the result array and leaving leaving it as byte
+                resultArray = cTranfrom.TransformFinalBlock((_input as byte[]), 0, (_input as byte[]).Length);
+                //Clearing
+                control.Clear();
+                //No need to convert so returning the bytes
+                return resultArray;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ encryptfile/encryptfile/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Security;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace encryptfile
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            //at is a thing we go to
+            at:
+            //The console writes a line that askes for a input e is for encrypting d is for decrypting
+            Console.WriteLine("Write e to encrypt and write d to decrypt");
+            //user is the input we get
+            string user = Console.ReadLine();
+            //Checking if the user wrote e for encrypting
+            if (user == "e")
+            {
+                //myCrypt is the class to encrypting and encrypthing
+                encryptfile.Class1 myCrypt = new Class1();
+                //Encryption key can be changed but has to be the same length
+                string key = "123456789portbv";
+                //Getting all the bytes from TestFile.txt (its a text file so the bytes are text)
+                byte[] fromFile = File.ReadAllBytes("TestFile.txt");
+                //Now it changes the bytes(text) to the encrypted text. As you can see we are using the myCrypt and we pass the agruments.
+                //We are doing ENCRYPT because the user wrote e and we are using AES but you can use TDES or rc2.
+                //We are passing the file bytes (text) and we use the key then we give the result as byte
+                File.WriteAllBytes("TestFile.txt",
+                    (myCrypt.Crypt(encryptfile.CryptMethod.ENCRYPT, encryptfile.CryptClass.AES, fromFile, key) as byte[]));
+            }
+            //User wrote d
+            if (user == "d")
+            {
+                // Making an myCrypt again
+                encryptfile.Class1 myCrypt = new Class1();
+                //The encryption key
+                string key = "123456789qwertyt";
+                //Everything asbefore but decrypting
+                byte[] fromFile = File.ReadAllBytes("TestFile.txt");
+                File.WriteAllBytes("TestFile.txt",
+                    (myCrypt.Crypt(encryptfile.CryptMethod.DECRYPT, encryptfile.CryptClass.AES, fromFile, key) as byte[]));
+            }
+            //going to at
+            goto at;
+
+        }
+    }
+}
